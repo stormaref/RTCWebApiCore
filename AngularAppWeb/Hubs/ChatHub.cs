4fa4bf1116357c26fe88ccd54b6500f98b3d419c
storm <storm.aref@gmail.com>
2019-08-18 12:14:09 +0430

    first commit

---
+++ AngularAppWeb/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace AngularAppWeb.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public Task Send(string message)
+        {
+            return Clients.All.SendAsync("Send", message);
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            return base.OnConnectedAsync();
+        }
+    }
+}
---
+++ AngularAppWeb/Models/Room.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AngularAppWeb.Models
+{
+    public class Room
+    {
+        private static readonly List<Room> Rooms = new List<Room>();
+
+        public string Name { get; set; }
+        public List<User> Users { get; set; } = new List<User>();
+
+        public static int TotalUsers => Rooms.Sum(room => room.Users.Count);
+
+        public static Room Get(string name)
+        {
+            lock (Rooms)
+            {
+                var current = Rooms.SingleOrDefault(r => r.Name == name);
+
+                if (current == default(Room))
+                {
+                    current = new Room
+                    {
+                        Name = name
+                    };
+                    Rooms.Add(current);
+                }
+
+                return current;
+            }
+        }
+    }
+}
